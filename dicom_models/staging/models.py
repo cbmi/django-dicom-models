d3dbae5147ea71d8d5e5d7dec5ea6c00a65f99ea
Jeffrey Miller <jeffmax@gmail.com>
2013-06-12 15:51:53 -0400

    Change suspect to high risk

--- dicom_models/staging/models.py
+++ dicom_models/staging/models.py
@@ -74,8 +74,8 @@ class RadiologyStudy(core.RadiologyStudy):
     pub_date = models.DateTimeField(null=True)
     study_date = models.DateTimeField(null=True)
     accession_no = models.CharField(max_length=20)
-    suspect_flag = models.BooleanField(default=False)
-    suspect_flag_message = models.CharField(null=True, max_length=500);
+    high_risk_flag = models.BooleanField(default=False)
+    high_risk_message = models.CharField(null=True, max_length=500);
 
     class Meta(object):
         verbose_name_plural = "Radiology Studies"
